377662e063754ad8d02a06f766bc5813b1d1e774
임빈영 <qlsdud960604@gmail.com>
2023-01-19 17:33:26 +0900

    SwaggerConfig 클래스 생성
    Swagger 설정을 위한 SwaggerConfig 클래스를 생성함

---
+++ src/main/java/com/example/swaggerbasic/config/SwaggerConfig.java
@@ -0,0 +1,63 @@
+package com.example.swaggerbasic.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    private String version;
+    private String title;
+
+    @Bean
+    public Docket apiV1() {
+        version = "V1";
+        title = "swagger-basic API" + version;
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .groupName(version)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(""))
+                .paths(PathSelectors.ant("/v1/api/**"))
+                .build()
+                .apiInfo(apiInfo(version, title));
+    }
+
+    @Bean
+    public Docket apiV2() {
+        version = "V2";
+        title = "swagger-basic API" + version;
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .groupName(version)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(""))
+                .paths(PathSelectors.ant("/v2/api/**"))
+                .build()
+                .apiInfo(apiInfo(version, title));
+    }
+
+    private ApiInfo apiInfo(String version, String title) {
+        return new ApiInfo(
+                title,
+                "Swagger로 생성한 API Docs",
+                version,
+                "www.example.com",
+                new Contact("Contact Me", "www.example.com", "foo@example.com"),
+                "Licenses",
+                "www.example.com",
+                new ArrayList<>()
+        );
+    }
+}
