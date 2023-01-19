e50239b142ae804bb5a360e3c48428100eb989bf
임빈영 <qlsdud960604@gmail.com>
2023-01-19 15:56:09 +0900

    프로젝트 생성 및 환경 설정

---
+++ src/main/java/com/example/swaggerbasic/SwaggerBasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.swaggerbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/swaggerbasic/SwaggerBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swaggerbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
