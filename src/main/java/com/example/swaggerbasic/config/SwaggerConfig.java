001714ebd8832e57c1cc41ad5917bf67d9f3fe07
임빈영 <qlsdud960604@gmail.com>
2023-01-30 13:57:59 +0900

    SwaggerConfig 클래스 수정
    각 API에 대한 공통 응답 메시지 설정

--- src/main/java/com/example/swaggerbasic/config/SwaggerConfig.java
+++ src/main/java/com/example/swaggerbasic/config/SwaggerConfig.java
@@ -2,15 +2,19 @@ package com.example.swaggerbasic.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -24,7 +28,7 @@ public class SwaggerConfig {
         title = "swagger-basic API " + version;
 
         return new Docket(DocumentationType.SWAGGER_2)
-                .useDefaultResponseMessages(false)
+                .globalResponseMessage(RequestMethod.GET, getResponseMessages())
                 .groupName(version)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage(""))
@@ -59,5 +63,26 @@ public class SwaggerConfig {
                 "www.example.com",
                 new ArrayList<>()
         );
+    }
+
+    private List<ResponseMessage> getResponseMessages() {
+        List<ResponseMessage> responseMessages = new ArrayList<>();
+
+        responseMessages.add(new ResponseMessageBuilder()
+                .code(200)
+                .message("OK")
+                .build());
+
+        responseMessages.add(new ResponseMessageBuilder()
+                .code(404)
+                .message("Not Found")
+                .build());
+
+        responseMessages.add(new ResponseMessageBuilder()
+                .code(500)
+                .message("Internal Server Error")
+                .build());
+
+        return responseMessages;
     }
 }
